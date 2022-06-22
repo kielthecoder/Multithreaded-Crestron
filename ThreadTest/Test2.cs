be3058450d227050210e9c55fa1ea6fc368ac880
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2022-06-22 11:16:01 -0400

    added Test2 class

---
+++ ThreadTest/Test2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace ThreadTest
+{
+    public class Test2
+    {
+        private static CMutex _lock;
+        private static int _counter;
+
+        private string _name;
+        private int _frequency;
+        private bool _running;
+
+        public event EventHandler<TimerEventArgs> TimerEvent;
+
+        public Test2()
+        {
+            if (_lock == null)
+            {
+                _lock = new CMutex();
+            }
+        }
+
+        public void Initialize(string name, short frequency)
+        {
+            _name = name;
+            _frequency = (int)frequency;
+        }
+
+        public void Start()
+        {
+            if (!_running)
+            {
+                _running = true;
+                CrestronInvoke.BeginInvoke(DoWork);
+            }
+        }
+
+        public void Stop()
+        {
+            if (_running)
+            {
+                _running = false;
+            }
+        }
+
+        private void DoWork(object userObj)
+        {
+            while (_running)
+            {
+                CrestronEnvironment.Sleep(_frequency);
+
+                if (_running)
+                {
+                    try
+                    {
+                        if (_lock.WaitForMutex(100))
+                        {
+                            _counter++;
+
+                            if (TimerEvent != null)
+                            {
+                                TimerEvent(userObj,
+                                    new TimerEventArgs(String.Format("{0}: counter = {1}",
+                                        _name, _counter)));
+                            }
+
+                            _lock.ReleaseMutex();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        CrestronConsole.PrintLine("Exception in {0}.DoWork: {1}",
+                            _name,
+                            e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
