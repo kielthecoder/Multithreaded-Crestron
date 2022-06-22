f5e307e70d615c69c3fc090d11693c3ff0f0d0b7
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2022-06-22 09:25:21 -0400

    added ThreadTest from working directory

---
+++ ThreadTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ThreadTest")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
---
+++ ThreadTest/Test1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Crestron.SimplSharp;
+
+namespace ThreadTest
+{
+    public class TimerEventArgs : EventArgs
+    {
+        public string Message;
+
+        public TimerEventArgs()
+        {
+            Message = "No message.";
+        }
+
+        public TimerEventArgs(string msg)
+        {
+            Message = msg;
+        }
+    }
+
+    public class Test1
+    {
+        private CTimer _timer;
+        private string _name;
+        private long _frequency;
+        private long _counter;
+        private bool _running;
+
+        public event EventHandler<TimerEventArgs> TimerEvent;
+
+        public Test1()
+        {
+        }
+
+        public void Initialize(string name, short frequency)
+        {
+            _name = name;
+            _frequency = frequency;
+            _timer = new CTimer(DoWork, this, Timeout.Infinite);
+        }
+
+        public void Start()
+        {
+            if (!_running)
+            {
+                _running = true;
+                _counter = 0;
+                _timer.Reset(_frequency, _frequency);
+            }
+        }
+
+        public void Stop()
+        {
+            if (_running)
+            {
+                _running = false;
+                _timer.Stop();
+            }
+        }
+
+        private void DoWork(object userObj)
+        {
+            if (_running)
+            {
+                _counter++;
+
+                if (TimerEvent != null)
+                {
+                    TimerEvent(userObj, new TimerEventArgs(String.Format("{0}: counting {1}", _name, _counter)));
+                }
+            }
+        }
+    }
+}
